d0aee98e61d33199683fecb22b933c0046d3aee6
wangzhongyong <wzy@kgtdata.com>
2022-07-19 11:18:49 +0800

    feat: 集成antd-design-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,11 +11,92 @@
     <button v-on:click="handleClick">click</button>
      <button v-on:click="add({num:1000})">click1</button>
      <button v-on:click="add({num:100})">click2</button>
+      <div>
+    <h1>antd</h1>
+     <a-date-picker v-bind:value="value1" @change="onChange"/>
+      <a-table :columns="columns" :data-source="data">
+    <a slot="name" slot-scope="text">{{ text }}</a>
+    <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+    
+    <span slot="tags" slot-scope="tags">
+      <a-tag
+        v-for="tag in tags"
+        :key="tag"
+        :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+      >
+        {{ tag.toUpperCase() }}
+      </a-tag>
+    </span>
+   
+   <span slot="action" slot-scope="text, record">
+      <a>Invite 一 {{ record.name }}</a>
+      <a-divider type="vertical" />
+      <a>Delete</a>
+      <a-divider type="vertical" />
+      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+    </span>
+  </a-table>
   </div>
+  </div>
+ 
 </template>
 
 <script>
 import { mapState,mapMutations,mapGetters } from 'vuex';
+import moment from 'moment';
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    scopedSlots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 export default {
   name: 'HelloWorld',
   props: {
@@ -25,10 +106,21 @@ export default {
     ...mapState('helloStore',['num']),
     ...mapGetters('helloStore',['newNum'])
   },
+  data(){
+  return {
+      value1:moment(),
+      data,
+      columns
+  }
+  },  
   mounted(){
     console.log('did',this);
   },
   methods:{
+    onChange(date, dateString){
+      console.log('date',date,'dateString',dateString);
+      this.value1 = date;
+    },  
     handleClick(){
       this.$store.commit("helloStore/add",{num:5})//muatations
       this.$store.dispatch("helloStore/add",{num:4})
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue';
 import store from './store/index';
 import router from './router/index';
 import Element from 'element-ui';
+import Antd from 'ant-design-vue';
 import 'element-ui/lib/theme-chalk/index.css';
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
+
 Vue.config.productionTip = false;
 Vue.use(Element);
+Vue.use(Antd);
 new Vue({ store, router, render: h => h(App) }).$mount('#app');
