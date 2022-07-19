ae7b1b129b915c07d00a16efe8f43da0b127a64a
wangzhongyong <wzy@kgtdata.com>
2022-07-19 13:41:36 +0800

    fix: app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-=======
   <ConfigProvider>
     <router-view />
   </ConfigProvider>
@@ -24,19 +9,10 @@ import { ConfigProvider } from 'ant-design-vue';
 import { useUserTheme } from './hooks/useTheme';
 
 useUserTheme();
->>>>>>> c1585c89fc703b29ae78355ec86b94b88c46dbee
 </script>
 
 <style>
 #app {
-<<<<<<< HEAD
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-=======
   height: 100%;
 }
 
@@ -45,14 +21,6 @@ useUserTheme();
 }
 
 .slide-left-enter-active,
-.slide-left-leave-active,
-.slide-right-enter-active,
-.slide-right-leave-active {
-  transition-duration: 0.5s;
-  transition-property: height, opacity, transform;
-  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
-  overflow: hidden;
-}
 
 .slide-left-enter,
 .slide-right-leave-active {
@@ -96,6 +64,5 @@ useUserTheme();
     opacity: 0;
     transform: scale3d(0.95, 0.95, 0.95);
   }
->>>>>>> c1585c89fc703b29ae78355ec86b94b88c46dbee
 }
 </style>
