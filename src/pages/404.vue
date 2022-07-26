b88e083d4e64c5cabbd6a28bfc7f9ba0588d05e0
wangzhongyong <wzy@kgtdata.com>
2022-07-26 14:25:22 +0800

    init

--- src/pages/404.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <el-result icon="error" title="404" sub-title="页面找不到"></el-result>
-  </div>
-</template>
--- src/stores/index.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'vuex';
-import doc from './doc';
-const store = createStore({
-    modules: {
-        doc
-    }
-});
-export default store;
