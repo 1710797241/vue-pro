49f1e4a668e6dd1e54a351d85844811ee687b13a
wangzhongyong <wzy@kgtdata.com>
2022-08-10 14:14:18 +0800

    chore: upgrade vite@3.0.5

---
+++ src/components/DefineComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>defineComponent</h1>
+  <h2>{name}</h2>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    title: String,
+
+  },
+  mounted() {
+
+  },
+  unmounted() {
+
+  },
+  computed: {
+
+  },
+  data() {
+    return {
+      name: '123456'
+    }
+  }
+})
+</script>
+
+<style>
+</style>
