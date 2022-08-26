ae348b4847ec66ffaa89dd67d802cb0f54e83dd7
wangzhongyong <wzy@kgtdata.com>
2022-08-26 14:55:04 +0800

    add packages

--- src/h5/App.tsx
+++ src/h5/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+
 import './App.css';
 import 'antd/dist/antd.css';
 import { StepForwardOutlined } from '@ant-design/icons';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react(), splitVendorChunkPlugin()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                h5: resolve(__dirname, 'h5/index.html')
+            }
+        }
+    }
+});
