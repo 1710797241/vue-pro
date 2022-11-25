a744d09502a184107a7e511e76e68f81e7b4082a
qiye <1710797241@qq.com>
2022-11-25 14:56:41 +0800

    chore: add inspector

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,15 @@ import { createVuePlugin } from 'vite-plugin-vue2';
 import legacy from '@vitejs/plugin-legacy';
 import vueJsx from '@vitejs/plugin-vue2-jsx';
 import proxy from './config/proxy';
+import Inspector from 'vite-plugin-vue-inspector';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         createVuePlugin(),
+        Inspector({
+            vue: 2
+        }),
         vueJsx(),
 
         legacy({
