eb066ee5b349012e2249e4f88edfd084a9001ac9
qiye <1710797241@qq.com>
2022-12-14 22:18:41 +0800

    el-form model无法传入

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -34,8 +34,21 @@ const Index = {
                 type: [],
                 resource: '',
                 desc: ''
-            }
+            },
+            elVals: { age: 1 }
         };
+    },
+    watch: {
+        deep: true,
+        form: function (val) {
+            console.log('form val', val);
+        },
+        form: {
+            handler(newVal, oldVal) {
+                console.log('form val', newVal);
+            },
+            deep: true
+        }
     },
     methods: {
         toggleSelection(rows) {
@@ -69,6 +82,7 @@ const Index = {
     render() {
         return (
             <div>
+                <div domPropsInnerHtml={'div'}></div>
                 <el-button onClick={this.handleVisible}>add</el-button>
                 <el-table
                     tooltip-effect="dark"
@@ -92,22 +106,22 @@ const Index = {
                     </el-button>
                 </div>
                 <el-dialog onClose={this.handleVisible} visible={this.visible}>
-                    <el-form ref="form" model={this.form} label-width="80px">
-                        <el-form-item label="活动名称">
-                            <el-input model={this.form.name}></el-input>
+                    <el-form ref="form" vModel={this.elVals} label-width="180px">
+                        {/* <el-form-item prop="name" label="活动名称">
+                            <el-input vModel={this.form.name}></el-input>
                         </el-form-item>
-                        <el-form-item label="活动区域">
-                            <el-select model="form.region" placeholder="请选择活动区域">
+                        <el-form-item prop="region" label="活动区域">
+                            <el-select vModel={this.form.region} placeholder="请选择活动区域">
                                 <el-option label="区域一" value="shanghai"></el-option>
                                 <el-option label="区域二" value="beijing"></el-option>
                             </el-select>
                         </el-form-item>
-                        <el-form-item label="活动时间">
+                        <el-form-item prop="date1" label="活动时间">
                             <el-col span={11}>
                                 <el-date-picker
                                     type="date"
                                     placeholder="选择日期"
-                                    model={this.form.date1}
+                                    vModel={this.form.date1}
                                     style="width: 100%;"
                                 ></el-date-picker>
                             </el-col>
@@ -117,32 +131,42 @@ const Index = {
                             <el-col span={11}>
                                 <el-time-picker
                                     placeholder="选择时间"
-                                    model={this.form.date2}
+                                    vModel={this.form.date2}
                                     style="width: 100%;"
                                 ></el-time-picker>
                             </el-col>
                         </el-form-item>
-                        <el-form-item label="即时配送">
-                            <el-switch model={this.form.delivery}></el-switch>
+                        <el-form-item prop="delivery" label="即时配送">
+                            <el-switch vModel={this.form.delivery}></el-switch>
                         </el-form-item>
-                        <el-form-item label="活动性质">
-                            <el-checkbox-group model="form.type">
+                        <el-form-item prop="type" label="活动性质">
+                            <el-checkbox-group vModel={this.form.type}>
                                 <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
                                 <el-checkbox label="地推活动" name="type"></el-checkbox>
                                 <el-checkbox label="线下主题活动" name="type"></el-checkbox>
                                 <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
                             </el-checkbox-group>
                         </el-form-item>
-                        <el-form-item label="特殊资源">
-                            <el-radio-group model={this.form.resource}>
+                        <el-form-item prop="resource" label="特殊资源">
+                            <el-radio-group vModel={this.form.resource}>
                                 <el-radio label="线上品牌商赞助"></el-radio>
                                 <el-radio label="线下场地免费"></el-radio>
                             </el-radio-group>
                         </el-form-item>
-                        <el-form-item label="活动形式">
-                            <el-input type="textarea" model={this.form.desc}></el-input>
+                        <el-form-item prop="desc" label="活动形式">
+                            <el-input type="textarea" vModel={this.form.desc}></el-input>
+                        </el-form-item> */}
+                        <el-form-item
+                            label="年龄"
+                            prop="age"
+                            rules={[
+                                { required: true, message: '年龄不能为空' },
+                                { type: 'number', message: '年龄必须为数字值' }
+                            ]}
+                        >
+                            <el-input vModel_number={this.form.age} autocomplete="off"></el-input>
                         </el-form-item>
-                        <el-form-item>
+                        <el-form-item prop="option">
                             <el-button type="primary" onClick={this.onSubmit}>
                                 立即创建
                             </el-button>
